75cb868a1365d44e914efefbc687890958ce7cb4
Luis Fernando <luis.fer160197@gmail.com>
2021-04-16 13:48:22 -0500

    Initial Commit

---
+++ foundations/js/Fundamentals-2/Switch/grades.js
@@ -0,0 +1,25 @@
+// Ranges
+// Set the student's grade
+const grade = 87;
+
+switch (true) {
+    // If score is 90 or greater
+    case grade >= 90:
+        console.log("A");
+        break;
+    // If score is 80 or greater
+    case grade >= 80:
+        console.log("B");
+        break;
+    // If score is 70 or greater
+    case grade >= 70:
+        console.log("C");
+        break;
+    // If score is 60 or greater
+    case grade >= 60:
+        console.log("D");
+        break;
+    // Anything 59 or below is failing
+    default:
+        console.log("F");
+}
---
+++ foundations/js/Fundamentals-2/Switch/week.js
@@ -0,0 +1,29 @@
+// Switch statement
+// Set the current day of the week to a variable, with 1 being Monday and 7 being Sunday
+const day = new Date().getDay();
+
+switch (day) {
+    case 1:
+        console.log("Happy Monday!");
+        break;
+    case 2:
+        console.log("It's Tuesday. You got this!");
+        break;
+    case 3:
+        console.log("Hump day already!");
+        break;
+    case 4:
+        console.log("Just one more day 'til the weekend!");
+        break;
+    case 5:
+        console.log("Happy Friday!");
+        break;
+    case 6:
+        console.log("Have a wonderful Saturday!");
+        break;
+    case 7:
+        console.log("It's Sunday, time to relax!");
+        break;
+    default:
+        console.log("Something went horribly wrong...");
+}
